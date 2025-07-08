6185777cf7b58c9a5b27ce8d1a0c914103242b33
chawki-ifaoui <chawki@zixdev.com>
2025-07-08 11:22:41 +0100

    Enhance ProductCategoryResource to support parent category filtering and improve category management; add header actions for main categories

--- Modules/Product/Filament/Admin/Resources/ProductCategoryResource.php
+++ Modules/Product/Filament/Admin/Resources/ProductCategoryResource.php
@@ -7,6 +7,7 @@ use Filament\Tables;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
 use Modules\Product\Entities\ProductCategory;
 use Modules\Product\Filament\Admin\Resources\ProductCategoryResource\Pages;
 
@@ -31,7 +32,9 @@ class ProductCategoryResource extends Resource
                             ->required(),
                         Forms\Components\Select::make('parent_id')
                             ->label(__('Parent Category'))
-                            ->relationship('parent', 'name')
+                            ->relationship('parent', 'name->en', function (Builder $query) {
+                                return $query->whereNull('parent_id');
+                            })
                             ->searchable()
                             ->preload(),
                         Forms\Components\Select::make('company_id')
@@ -62,7 +65,13 @@ class ProductCategoryResource extends Resource
                     ->label(__('Company')),
             ])
             ->filters([
-                //
+                Tables\Filters\SelectFilter::make('parent_id')
+                    ->label(__('Parent Category'))
+                    ->relationship('parent', 'name->' . app()->getLocale(), function (Builder $query) {
+                        return $query->whereNull('parent_id');
+                    })
+                    ->searchable()
+                    ->preload(),
             ])
             ->actions([
                 Tables\Actions\ActionGroup::make([
@@ -75,6 +84,28 @@ class ProductCategoryResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
+            ])
+            ->modifyQueryUsing(function (Builder $query) {
+                $query->when(
+                    request()->query('parent_id'),
+                    fn(Builder $query, $parentId) => $query->where('parent_id', $parentId),
+                    fn(Builder $query) => $query->whereNull('parent_id')
+                );
+            })
+            ->headerActions([
+                Tables\Actions\Action::make('all')
+                    ->label(__('All main categories'))
+                    ->url(fn() => route('filament.admin.resources.product-categories.index'))
+                    ->badge()
+                    ->color(request()->query('parent_id') ? 'gray' : 'primary'),
+                ...static::getParentCategories()->map(function ($category) {
+                    $isActive = request()->query('parent_id') == $category->id;
+                    return Tables\Actions\Action::make($category->id)
+                        ->label($category->getTranslation('name', app()->getLocale()))
+                        ->url(fn() => route('filament.admin.resources.product-categories.index', ['parent_id' => $category->id]))
+                        ->badge()
+                        ->color($isActive ? 'primary' : 'gray');
+                })->toArray(),
             ]);
     }
 
@@ -93,6 +124,11 @@ class ProductCategoryResource extends Resource
             'edit' => Pages\EditProductCategory::route('/{record}/edit'),
             'view' => Pages\ViewProductCategory::route('/{record}'),
         ];
+    }
+
+    protected static function getParentCategories()
+    {
+        return ProductCategory::whereNull('parent_id')->orderBy('order', 'asc')->get();
     }
 
     public static function getLabel(): ?string
